aa38071f7f85ad425611d38a391c824ba2577bf3
BRIAN SANDON (RIT Student) <bxs9775@ad.rit.edu>
2016-10-03 16:40:47 -0400

    levelManager now loads and draws the level. The levelManager.create() will need to be moved.

--- js/levelManager.js
+++ js/levelManager.js
@@ -8,7 +8,7 @@ app.levelManager = {
 	//delta: 0, //not sure if this is the right manager to keep track of this value.
 	//scrollSpeed: 5,
 	game: undefined,
-	level: undefined,
+	gameMap: undefined,
 	backgroundLayer: undefined,
 	collisionLayer: undefined,
 	
@@ -16,7 +16,7 @@ app.levelManager = {
 	init: function(){
 		this.game = app.main.game;
 		
-		console.log("levelManager initialized");
+		console.log("levelManager initialized.");
 	},
 	
 	//Preloads assets for the game - called by phaser on initialization of game
@@ -24,33 +24,34 @@ app.levelManager = {
 		//map data
 		this.game.load.tilemap('planet','assets/tilemaps/maps/testMap.json', null, Phaser.Tilemap.TILED_JSON);
 		//tileset
-		this.game.load.image('planetTiles','assets/tilemaps/tiles/testTiles.jpg');
+		this.game.load.image('planetTiles','assets/tilemaps/tiles/testTiles.png');
     },
 
     //Creates objects - called by phaser on initialization of game
     create: function(){
-		//set level
-		this.level = app.main.game.add.tilemap('planet');
-		this.level.addTilesetImage('testTiles','planetTiles');
 		
-		//set layers
-		this.backgroundLayer = this.level.createLayer('backgroundLayer');
-		this.collisionLayer = this.level.createLayer('collisionLayer');
+		//set level
+		this.gameMap = app.main.game.add.tilemap('planet');
+		this.gameMap.addTilesetImage('testTiles','planetTiles');
+		
+		//sets layers
+		this.backgroundLayer = this.gameMap.createLayer('backgroundLayer');
+		this.collisionLayer = this.gameMap.createLayer('collisionLayer');
 		
 		//set collision with the collisionLayer
-		this.level.setCollisionBetween(1,2000,true,'collisionLayer');
+		this.gameMap.setCollisionBetween(1,10000,true,'collisionLayer');
 		
-		this.backgroundLayer.resize();
+		this.backgroundLayer.resizeWorld();
 		
 		console.log("Level has been created.");
+		
     },
 	
     // Updates 1 step in the game state - called by phaser
 	update: function(){
-        //app.entityManager.update();
+
+        app.entityManager.update();
 		//delta = game.Time.now()-game.Time.prevTime();
-		
-		//[Collision handling occurs here?] 
 	},
 	
 	//levelManager specific functions
